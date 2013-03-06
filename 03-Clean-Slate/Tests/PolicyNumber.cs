39b6c5f4632307db68ce00e984c8285748f6681b
AdrianFreemantle <adrian@synerics.com>
2013-03-06 11:17:22 +0200

    Started with TDD based modeling exercise

---
+++ 03-Clean-Slate/Tests/PolicyNumber.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Core;
+
+namespace Tests
+{
+    public class PolicyNumber
+    {
+        public string Number { get; protected set; }
+
+        public PolicyNumber(string number)
+        {
+            Mandate.ParameterNotNullOrEmpty(number, "policyNumber");
+            Mandate.That(number.Length == 10);
+
+            Number = number;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            var identity = obj as PolicyNumber;
+
+            return identity != null && Equals(identity);
+        }
+
+        protected bool Equals(PolicyNumber other)
+        {
+            return string.Equals(Number, other.Number);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Number != null ? Number.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(PolicyNumber left, PolicyNumber right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(PolicyNumber left, PolicyNumber right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
---
+++ 03-Clean-Slate/Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Domain.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Shouldly;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Policies_with_the_same_policy_number_are_equal()
+        {
+            var first = new Policy(new PolicyNumber("0123456789"));
+            var second = new Policy(new PolicyNumber("0123456789"));
+
+            (first.PolicyNumber == second.PolicyNumber).ShouldBe(true);
+            first.PolicyNumber.Equals(second.PolicyNumber).ShouldBe(true);
+        }
+
+        /* Policy Creation(PlanCode xxx, MaimMember, Payour, premium, ) 
+         * 
+         */
+    }
+
+    public class Policy
+    {
+        public PolicyNumber PolicyNumber { get; set; }
+        
+        public Policy(PolicyNumber policyNumber)
+        {
+            Mandate.ParameterNotNull(policyNumber, "policyNumber");
+
+            PolicyNumber = policyNumber;
+        }
+    }
+}
